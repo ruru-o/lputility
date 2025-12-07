ef6d18bddfc838231f10d31ba7ad5051e4e91aa1
Joswen.Pinca <owenpinca2022@gmail.com>
2025-12-07 21:29:00 +0800

    functions.py

--- functions.py
+++ functions.py
@@ -12,31 +12,38 @@ class ToolkitFunctions:
         if software_key == "python":
             return self.installer.install_python()
         if software_key == "java":
-            return self.installer.install_python()
+            return self.installer.install_java()
         if software_key == "nodejs":
-            return self.installer.install_python()
+            return self.installer.install_nodejs()
         if software_key == "vscode":
-            return self.installer.install_python()
+            return self.installer.install_vscode()
         if software_key == "git":
-            return self.installer.install_python()
+            return self.installer.install_git()
         if software_key == "Android Studio":
-            return self.installer.install_python()
+            return self.installer.install_androidstudio()
         if software_key == "Tableau Public":
-            return self.installer.install_python()
-        if software_key == "python":
-            return self.installer.install_python()
-        if software_key == "python":
-            return self.installer.install_python()
-        if software_key == "python":
-            return self.installer.install_python()
-        if software_key == "python":
-            return self.installer.install_python()
-        if software_key == "python":
-            return self.installer.install_python()
+            return self.installer.install_tableaupublic()
+        if software_key == "Canva":
+            return self.installer.install_Canva()
+        if software_key == "GIMP":
+            return self.installer.install_GIMP()
+        if software_key == "Blender":
+            return self.installer.install_Blender()
+        if software_key == "Audacity":
+            return self.installer.install_Audacity()
+        if software_key == "Krita":
+            return self.installer.install_Krita()
+        if software_key == "Grammarly":
+            return self.installer.install_Grammarly()
+        if software_key == "CapCut":
+            return self.installer.install_Capcut()
+        if software_key == "Microsoft Teams":
+            return self.installer.install_Microsoft()
         
         # implement the rest dito mismo
         
 
         return False
+
 
 
