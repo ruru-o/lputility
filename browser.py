f5fd53646fffcb1351026cd08c7d337860a93acc
Joseph.Pimentel <jaspimentel31@gmail.com>
2025-12-10 05:47:40 +0800

    Update browser.py

--- browser.py
+++ browser.py
@@ -2,20 +2,20 @@
 
 import webbrowser
 
-class BrowserEmbedder:   
+class BrowserEmbedder:
     def __init__(self, logger):
         self.logger = logger
     
     def open_portal(self, url):
+        url = url.strip()  
         self.logger.log(f"Opening portal: {url}")
+
         
-        # add https:// if missing
-        if not url.startswith('http'):
+        if not url.lower().startswith(('http://', 'https://')):
             url = 'https://' + url
-        
+
         try:
             webbrowser.open(url)
             return True
-        except:
-
+        except Exception:
             return False
